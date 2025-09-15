f29f2dab83b0c635b87a662dfc1f642db286b161
Scott Hatfield <contact@hatfieldscott.com>
2025-09-14 21:38:33 -0500

    Created CLI from template

---
+++ bin/splendid_cli.dart
@@ -0,0 +1,5 @@
+import 'package:splendid_cli/splendid_cli.dart' as splendid_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${splendid_cli.calculate()}!');
+}
---
+++ test/splendid_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:splendid_cli/splendid_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
